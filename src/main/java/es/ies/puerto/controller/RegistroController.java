f21d5c9e4acb4a7be633584864664c422ad8b272
Alexander Faustino Diaz Bautista <AlexanderFaustinoDiazBautista@Gmail.com>
2025-03-27 10:07:14 +0000

    Update

--- src/main/java/es/ies/puerto/controller/RegistroController.java
+++ src/main/java/es/ies/puerto/controller/RegistroController.java
@@ -11,21 +11,29 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 /**
  * @author alexfdb
  * @version 1.0.0
  */
 public class RegistroController {
 
-    @FXML private TextField textFiledIngresarUsuario;
-    @FXML private PasswordField passwordFieldIngresarPassword;
-    @FXML private PasswordField paswordFieldRepetirPassword;
-    @FXML private TextField textFiledIngresarNombre;
-    @FXML private TextField textFiledIngresarEmail;
-    @FXML private TextField textFiledRepetirEmail;
-    @FXML private Text textMensaje;
-    @FXML private Button buttonRegistrar;
-    @FXML private Button openLoginButton;
+    @FXML
+    private TextField textFiledIngresarUsuario;
+    @FXML
+    private PasswordField passwordFieldIngresarContrasenia;
+    @FXML
+    private PasswordField paswordFieldRepetirContrasenia;
+    @FXML
+    private TextField textFiledIngresarNombre;
+    @FXML
+    private TextField textFiledIngresarEmail;
+    @FXML
+    private Text textMensaje;
+    @FXML
+    private Button buttonEnviar;
+    @FXML
+    private Button buttonInicio;
 
     private UsuarioCrud usuarioCrud;
 
@@ -33,18 +41,19 @@ public class RegistroController {
      * Constructor general.
      */
     public RegistroController() {
-        this.usuarioCrud = new UsuarioCrud(); 
+        this.usuarioCrud = new UsuarioCrud();
     }
 
     /**
      * Ejecuta el registro.
      */
-    @FXML protected void buttonRegistrarClick() {
+    @FXML
+    protected void buttonEnviarClick() {
 
         Usuario usuario = new Usuario(textFiledIngresarUsuario.getText(),
-                                      passwordFieldIngresarPassword.getText(),
-                                      textFiledIngresarNombre.getText(),
-                                      textFiledIngresarEmail.getText());
+                passwordFieldIngresarContrasenia.getText(),
+                textFiledIngresarNombre.getText(),
+                textFiledIngresarEmail.getText());
 
         usuarioCrud.create(usuario);
 
@@ -54,18 +63,19 @@ public class RegistroController {
     /**
      * Cambia a la pantalla principal.
      */
-    @FXML protected void openLoginClick() {
+    @FXML
+    protected void buttonInicioClick() {
 
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(PrincipalApplication.class.getResource("login.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 250, 400);
-            Stage stage = (Stage) openLoginButton.getScene().getWindow();
+            Stage stage = (Stage) buttonInicio.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
 
 }
